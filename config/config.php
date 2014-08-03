dbc3a953b51f8240d56a3fa8192d71eabcaee6ab
SrPatinhas <migascc69@gmail.com>
2014-08-03 04:31:06 +0100

    update all files - fase 1 - delete

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$bd_maquina 	= "localhost";
-	$bd_utilizador 	= "root";
-	$bd_senha 		= "1234";
-	$bd_base 		= "my_note";
-
-	$mydb = @new mysqli($bd_maquina, $bd_utilizador, $bd_senha, $bd_base); 
-	$mydb -> set_charset("utf8");
-?>
--- ext/Facebook_Twitter_Login/config/fbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-define('APP_ID', '1388696308017573');
-define('APP_SECRET', 'e5bc11b8a6dad759b63c8827a740314b');
-
-
-?>
